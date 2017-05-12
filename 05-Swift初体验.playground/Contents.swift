a252262bfa2deecf7235e546d1a748c826e6baf5
崔兵兵 <1655902151@qq.com>
2017-05-12 11:30:23 +0800

    类属性：存储属性、计算属性、类属性<static修饰>

--- 05-Swift初体验.playground/Contents.swift
+++ 05-Swift初体验.playground/Contents.swift
@@ -15,3 +15,41 @@ class Person {
 let p = Person()
 p.name = "cui"
 p.age = 18
+
+
+//  定义类的属性
+class Student {
+    //  存储属性：用于存储实例的常量&变量
+    var name : String = ""
+    var age : Int = 0
+    var mathScore : Double = 0.0
+    var chineseScore : Double = 0.0
+    
+    //  计算属性：通过某种方式计算得来结果的属性，就是计算属性
+    var averageScore : Double {
+        return (mathScore + chineseScore) * 0.5
+    }
+    
+    //  类属性 <通过static修饰>
+    static var courseCount : Int = 0
+}
+
+//  创建类的对象
+let stu = Student()
+
+stu.name = "cui"
+stu.age = 18
+stu.mathScore = 80
+stu.chineseScore = 90
+
+//  获取某一学生的平均成绩
+let averageScore = stu.averageScore
+print(averageScore)
+
+//  访问类属性
+Student.courseCount = 2
+
+
+
+
+
